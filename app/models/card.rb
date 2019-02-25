53e5863cd7a697818c98b91a377e3f42e5d5eeeb
Kohei Shinsawa <kshinsawa@netprotections.co.jp>
2019-02-25 09:38:52 +0900

    create split cards logic

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,3 +1,23 @@
-class Card < ApplicationRecord]
+class Card < ApplicationRecord
+  POKER_HAND = ["High Card", "One Pair", "Two Pair", "Three of a Kind",
+                "Straight", "Flush", "Full House", "Four of a Kind", "Straight Flush"]
+  attr_accessor :cards, :card, :suit, :number, :number_of_sets, :flush, :straight, :hand, :strength
 
+  def split_cards
+    @card = @cards.split(" ")
+  end
+
+  def split_card_into_suit
+    @suit = []
+    @card.each do |card|
+      @suit.push card[0]
+    end
+  end
+
+  def split_card_into_number
+    @number = []
+    @card.each do |card|
+      @number.push card[1..-1].to_i
+    end
+  end
 end
