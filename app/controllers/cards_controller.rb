532b7d6eb45226016222277b30988978138ed085
Kohei Shinsawa <kshinsawa@netprotections.co.jp>
2019-02-25 09:47:44 +0900

    create check method

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -1,10 +1,32 @@
 class CardsController < ApplicationController
   def top
+    @cards = Card.new
   end
 
   def result
   end
 
   def error
+  end
+
+  def check
+    @cards = Card.new(card_params)
+    if @cards.save
+      @cards.split_cards
+      @cards.split_card_into_suit
+      @cards.split_card_into_number
+      @cards.count_same_number
+      @cards.flush?
+      @cards.straight?
+      @cards.judge_hand
+      render :result
+    else
+      render :error
+    end
+  end
+
+  private
+  def card_params
+    params.require(:card).permit(:cards)
   end
 end
