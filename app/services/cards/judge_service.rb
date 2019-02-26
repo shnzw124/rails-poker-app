1786b7b423f8032ace3ae81604ff20563c18c589
Kohei Shinsawa <kshinsawa@netprotections.co.jp>
2019-02-26 14:43:29 +0900

    modify judge method

--- app/services/cards/judge_service.rb
+++ app/services/cards/judge_service.rb
@@ -19,34 +19,58 @@ class JudgeService
   end
 
   def judge
-    @cards = @card_set.split(" ")
+    split_cards
+    split_card_into_suit
+    split_card_into_number
+    count_same_number
+    flush?
+    straight?
+    judge_hand
+    judge_strength
+  end
 
+  private
+  def split_cards()
+    @cards = @card_set.split(" ")
+  end
+
+  def split_card_into_suit()
     @suits = []
     @cards.each do |card|
       @suits.push card[0]
     end
+  end
 
+  def split_card_into_number()
     @numbers = []
     @cards.each do |card|
       @numbers.push card[1..-1].to_i
     end
+  end
 
+  def count_same_number()
     @number_set = @numbers.group_by{|number|number}.map{|key, value|value.size }.sort.reverse
+  end
 
+  def flush?()
     variety = @suits.uniq.size
     if variety == 1
       @flush = true
     else
       @flush = false
     end
+  end
 
+  def straight?()
     steps = @numbers.sort.map{|number|number - @numbers[0]}
     if steps == [0,1,2,3,4] || steps == [0,9,10,11,12]
       @straight = true
     else
       @straight = false
     end
+  end
 
+  def judge_hand()
     case [@straight, @flush]
     when [true, true]
       @hand = POKER_HAND[8]
@@ -70,83 +94,9 @@ class JudgeService
         @hand = POKER_HAND[0]
       end
     end
-
-    # split_cards
-    # split_card_into_suit
-    # split_card_into_number
-    # count_same_number
-    # flush?
-    # straight?
-    # judge_hand
-    # judge_strength
   end
 
-  private
-  def split_cards(card_set)
-    @cards = card_set.split(" ")
-  end
-
-  def split_card_into_suit
-    cards.each do |card|
-      suits.push card[0]
-    end
-  end
-
-  def split_card_into_number
-    cards.each do |card|
-      numbers.push card[1..-1].to_i
-    end
-  end
-
-  def count_same_number(numbers)
-    number_set = numbers.group_by{|number|number}.map{|key, value|value.size }.sort.reverse
-  end
-
-  def flush?(suits)
-    variety = suits.uniq.size
-    if variety == 1
-      flush = true
-    else
-      flush = false
-    end
-  end
-
-  def straight?(numbers)
-    steps = numbers.sort.map{|number|number - @number[0]}
-    if steps == [-4,-3,-2,-1,0] || steps == [0,9,10,11,12]
-      straight = true
-    else
-      straight = false
-    end
-  end
-
-  def judge_hand
-    case [straight, flush]
-    when [true, true]
-      hand = POKER_HAND[8]
-    when [false, true]
-      hand = POKER_HAND[5]
-    when [true, false]
-      hand = POKER_HAND[4]
-    else
-      case number_set
-      when [4, 1]
-        hand = POKER_HAND[7]
-      when [3, 2]
-        hand = POKER_HAND[6]
-      when [3, 1, 1]
-        hand = POKER_HAND[3]
-      when [2, 2, 1]
-        hand = POKER_HAND[2]
-      when [2, 1, 1, 1]
-        hand = POKER_HAND[1]
-      else
-        hand = POKER_HAND[0]
-      end
-    end
-  end
-
-  def judge_strength
-    strength = POKER_HAND.index(hand)
+  def judge_strength()
+    @strength = POKER_HAND.index(@hand)
   end
 end
