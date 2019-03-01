3d03b2a1068db15ee0026d423530956ff90b82f6
Kohei Shinsawa <kshinsawa@netprotections.co.jp>
2019-03-01 11:39:45 +0900

    add rspec

---
+++ spec/controllers/cards_controller_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+describe CardsController do
+
+  describe 'Get #top' do
+    before do
+      get :top
+    end
+    it 'リクエストは200 OKとなること' do
+      expect(response.status).to eq 200
+    end
+    it '@cardに新しい手札を割り当てること' do
+      expect(assigns(:card)).to be_a_new(JudgeService)
+    end
+    it ':topテンプレートを表示すること' do
+      expect(response).to render_template :top
+    end
+  end
+
+end
