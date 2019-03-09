c67f2f338a4de9ab7fc0d9fb5bd6e97ec8fe8465
Kohei Shinsawa <kshinsawa@netprotections.co.jp>
2019-03-09 14:27:32 +0900

    add error handling

--- app/apis/api/v1/cards/check.rb
+++ app/apis/api/v1/cards/check.rb
@@ -3,12 +3,29 @@ module API
     module Cards
       class Check < Grape::API
         resource :cards do
+          # rescue_fromは書いた順番にマッチングされる
+
+          # 400 Bad Request
+          rescue_from Grape::Exceptions::Base do
+            error!(error: "400 Bad Request：不正なリクエストです")
+          end
+
+          # 404 Not Found
+          route :any, '*path' do
+            error!(error: "404 Not Found：指定されたURLは存在しません")
+          end
+
+          # 500 Internal Server Error
+          rescue_from Exception do
+            error!(error: "500 Internal Server Error：予期しないエラーです")
+          end
 
           # POST /api/v1/cards/check
           desc 'Judge hand, strength and best hand'
           params do
             requires :cards, type: Array[String]
           end
+
           post :check, jbuilder:'cards/check' do
             # Json → Arrayにパースして配列から一つずつeachで取り出して役判定＋強さ判定 → 最終的に強さを比較して最も強いものにbest = trueで返す
 
