d13d52f253b5e48ce35451e4c0c74460a52559ec
shnzw124 <shnzw124.com@gmail.com>
2021-10-28 02:00:46 +0900

    modify routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,7 @@ Rails.application.routes.draw do
   get root to: 'cards#top'
   get  '/result', to: 'cards#result'
   get  '/error', to: 'cards#error'
-  post '/check', to: 'cards#check'
-  patch '/check', to: 'cards#check'
+  post '/', to: 'cards#check'
   mount API::Root => '/'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
