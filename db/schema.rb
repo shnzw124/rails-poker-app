2b5f021f224041adb2b4830b2d7cec5d9751f9ea
shnzw124 <shnzw124.com@gmail.com>
2021-10-28 08:56:56 +0900

    modify

--- db/schema.rb
+++ db/schema.rb
@@ -12,4 +12,7 @@
 
 ActiveRecord::Schema.define(version: 2019_02_26_054643) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
 end
